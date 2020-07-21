2cb28e8d54543b51b91401801e19b3d104888787
ifeanyi2667 <ifeanyifestus@gmail.com>
2020-07-21 16:22:17 +0100

    more changes

---
+++ Public - Copy/assets/js/main.js
@@ -0,0 +1,27 @@
+//   Initializing feather icon
+feather.replace();
+
+const body = document.querySelector("body");
+
+const navbar = document.querySelector("#navbar");
+
+const navClose = document.querySelector("#nav-toggler-close");
+const navOpen = document.querySelector("#nav-toggler");
+
+// Nav bar Toggling
+navOpen.addEventListener("click", (evt) => {
+  body.classList.toggle("mobile-view");
+});
+
+navClose.addEventListener("click", (evt) => {
+  body.classList.toggle("mobile-view");
+});
+
+// Sticky Navbar
+window.addEventListener("scroll", (evt) => {
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("y-scrolling");
+  } else {
+    navbar.classList.remove("y-scrolling");
+  }
+});
